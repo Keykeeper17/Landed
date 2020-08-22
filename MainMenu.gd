7e6f0aab120d99c763e7d4ec31157890ebd63b47
Keykeeper17 <51512161+Keykeeper17@users.noreply.github.com>
2020-08-22 10:33:59 -0400

    Other Main Menu Controls work now

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _on_Exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_Controls_pressed() -> void:
+	get_tree().change_scene('res://ControlsScene.tscn')
+
+
+func _on_Play_pressed() -> void:
+	get_tree().change_scene('res://FadeIn.tscn')
